d100c81cf69ef2d18cbde431679031bd726116a1
ValeryShvyndzikau <valery_shvyndzikau@epam.com>
2020-04-17 17:56:09 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# position-errors-service
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/position-errors-service)
---
+++ index.ts
@@ -0,0 +1,202 @@
+  
+  import {first, last, get, eq, isEmpty, find, reduce, split, replace} from 'lodash';
+
+  
+  const newPosition = {
+  "name": "",
+  "positionCustomData": [
+    {
+      "name": "Only Custom Data Field",
+      "value": "fgfhf%"
+    },
+    {
+      "name": "Seniority",
+      "value": "wwwwwwwwwwwwwwwwwwwddddddddddddddwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww"
+    }
+  ],
+  "isExempt": false,
+  "locations": [
+    {
+      "effectiveDate": "2020-04-17",
+      "laborCategory": null,
+      "primaryJob": null
+    },
+    {
+      "effectiveDate": "1900-01-01",
+      "laborCategory": "",
+      "primaryJob": ""
+    }
+  ],
+  "positionStatuses": [
+    {
+      "name": "Active",
+      "effectiveDate": "2020-04-17"
+    },
+    {
+      "name": "Inactive",
+      "effectiveDate": null
+    },
+    {
+      "name": null,
+      "effectiveDate": "2020-04-24"
+    }
+  ],
+  "hireDate": "2020-04-17"
+}
+
+  const validationResult = [
+  {
+    "path": "name",
+    "strategy": "required",
+    "invalid": true
+  },
+  {
+    "path": "positionCustomData.0.value",
+    "strategy": "reg_exp",
+    "invalid": true
+  },
+  {
+    "path": "positionCustomData.1.value",
+    "strategy": "max_length",
+    "invalid": true
+  },
+  {
+    "path": "locations.0.primaryJob",
+    "strategy": "required",
+    "invalid": true
+  },
+  {
+    "path": "positionStatuses.1.effectiveDate",
+    "strategy": "required",
+    "invalid": true
+  },
+  {
+    "path": "positionStatuses.2.name",
+    "strategy": "required",
+    "invalid": true
+  }
+]
+
+/*
+
+html.pe_section_prefix_name = 'Position Name'
+html.pe_section_prefix_name = 'Position Name'
+html.pe_section_prefix_name = 'Position Name'
+
+*/
+
+
+function createMapOfParents(validationResult, data) {
+
+  const r = reduce(validationResult, (acc, current) => {
+
+    const sectionName = first(split(current.path, '.'));
+    const fieldName = last(split(current.path, '.'));
+
+
+
+    const adjusted = {
+      ...current,
+      path: current.path,
+      sectionName,
+      fieldName: eq(sectionName, 'positionCustomData') ? getDynamicFieldName(current.path, data) : fieldName
+    }
+
+
+    return {
+      ...acc,
+      [sectionName]: isEmpty(acc[sectionName]) ? [adjusted] : [...acc[sectionName], adjusted]
+    }
+
+  }, {})
+
+  return r;
+
+}
+
+
+function getDynamicFieldName(path, data) {
+
+  const p = replace(path, 'value', 'name');
+  console.log(p, 'p')
+
+  return get(data, p)
+
+
+}
+
+type Error = {
+
+}
+
+class PositionErrorsService {
+
+  propertyFilter = (value) => value;
+
+  // public normalize(position, errors): NormalizedErrors {
+
+  // }
+
+  public combineToLocalizedError(errors): Error {
+
+    /*
+    flow([
+      () => this.formatAndLocalize(),
+      (formattedErrors) => this.combineToSingleError()
+    ])
+
+
+    */
+
+  }
+
+  private formatAndLocalize(errors, position) {
+
+    const r = reduce(errors, (acc, error) => {
+
+      const pathSegments = split(error.path, '.');
+      const sectionName = first(pathSegments);
+      const fieldName = last(pathSegments);
+
+      const formattedError = {
+        ...error,
+        sectionName,
+        // TODO: Move to constant
+        fieldName: eq(sectionName, 'positionCustomData') ? getDynamicFieldName(error.path, position) : fieldName
+      }
+
+      return {
+        ...acc,
+        [sectionName]: isEmpty(acc[sectionName]) ? [formattedError] : [...acc[sectionName], formattedError]
+      }
+
+  }, {})
+
+  return r;
+  }
+
+  private getSectionName(path: string[]): string {
+    return first(split(path, '.'));
+  }
+
+  private getFieldName(path: string[]): string {
+    return last(split(path, '.'));
+  }
+
+  private localizeSection(sectionName) {
+    return this.propertyFilter(`html.multiplePositions.editor.validation.sections.${sectionName}`)
+  }
+
+  private localizeField(value) {
+
+    return 'field_pefix' + value;
+
+  }
+
+
+
+}
+
+
+
+console.log(createMapOfParents(validationResult, newPosition), 'vr')
