0f77b980b7da42d8cbae848cfd44346461ff8545
ValeryShvyndzikau <valery_shvyndzikau@epam.com>
2020-04-19 21:48:11 +0300

    1 file modified

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
   
-  import {map, includes, flow, first, last, get, eq, isEmpty, find, reduce, split, replace, uniqWith, uniqBy, matches} from 'lodash';
+  import {map, join, includes, concat, flow, first, last, get, eq, isEmpty, find, reduce, split, replace, uniqWith, uniqBy, matches} from 'lodash';
 
 
+console.log(concat([], 'abc'), 'test concat')
 
 
 const collection = [
@@ -11,7 +12,7 @@ const collection = [
   {id: 4, x: 2, y: 2}
 ]
 
-console.log(uniqBy(collection, 'x'))
+//console.log(uniqBy(collection, 'x'))
 
 
 
@@ -43,15 +44,15 @@ console.log(uniqBy(collection, 'x'))
   ],
   "positionStatuses": [
     {
-      "name": "Active",
+      "status": "Active",
       "effectiveDate": "2020-04-17"
     },
     {
-      "name": "Inactive",
+      "status": "Inactive",
       "effectiveDate": null
     },
     {
-      "name": null,
+      "status": null,
       "effectiveDate": "2020-04-24"
     }
   ],
@@ -90,7 +91,7 @@ console.log(uniqBy(collection, 'x'))
     "invalid": true
   },
   {
-    "path": "positionStatuses.2.name",
+    "path": "positionStatuses.2.status",
     "strategy": "required",
     "invalid": true
   }
@@ -105,156 +106,70 @@ html.pe_section_prefix_name = 'Position Name'
 */
 
 
-function createMapOfParents(validationResult, data) {
-
-  const r = reduce(validationResult, (acc, current) => {
-
-    const sectionName = first(split(current.path, '.'));
-    const fieldName = last(split(current.path, '.'));
-
-
-
-    const adjusted = {
-      ...current,
-      path: current.path,
-      sectionName,
-      fieldName: eq(sectionName, 'positionCustomData') ? getDynamicFieldName(current.path, data) : fieldName
-    }
-
-
-    return {
-      ...acc,
-      [sectionName]: isEmpty(acc[sectionName]) ? [adjusted] : [...acc[sectionName], adjusted]
-    }
-
-  }, {})
-
-  return r;
-
-}
-
-
-function getDynamicFieldName(path, data) {
-
-  const p = replace(path, 'value', 'name');
-  console.log(p, 'p')
-
-  return get(data, p)
-
-
-}
-
-type Error = {
-
-}
+type Error = {};
 
 class PositionErrorsAdapter {
 
   propertyFilter = (value) => value;
 
-  // public normalize(position, errors): NormalizedErrors {
+  public transformToErrorInterface(errors, position): Error {
 
-  // }
-
-  public buildLocalizedError(errors, position) {
-      // return flow([
-      //   () => this.format(errors, position),
-      //   (formatted) => {
-      //    return this.localize()
-      //   },
-      //   (localized) => this.combineToSingleError()
-      // ])();
+    return flow([
+      () => this.localize(errors, position),
+      (localizedErrors) => this.groupBySections(localizedErrors),
+      (grouppedErrors) => this.buildToSingleMessage(grouppedErrors)
+    ])();
   }
 
-  public transformToErrorInterface(errors): Error {
-
-    /*
-    flow([
-      () => this.format(),
-      (formatted) => this.localize(),
-      (localized) => this.combineToSingleError()
-    ])
-
-    flow2([
-      () => this.localize(), // flat array via map
-      (localized) => this.groupBySections(),
-      (groupped) => this.combineToSingleErrorInterface()
-    ])
-
-
-    */
-
+  private localize(errors, position) { 
+    return map(errors, ({path, strategy}) => ({
+      description: this.localizeStrategy(strategy),
+      sectionName: this.localizeSection(path),
+      fieldName: (
+        includes(path, 'positionCustomData')
+          ? this.getCustomDataFieldName(path, position)
+          : this.localizeField(path)
+      )
+    }));
   }
 
-  private format(errors, position) {
-
-    const r = reduce(errors, (acc, error) => {
-
-      //const pathSegments = split(error.path, '.');
-      // TODO: Consider to rename to id
-      const sectionName = this.getSectionName(error.path);
-      const fieldName = this.getFieldName(error.path);
-
-      const formattedError = {
-        ...error,
-        sectionName,
-        // TODO: Move to constant
-        fieldName: eq(sectionName, 'positionCustomData') ? getDynamicFieldName(error.path, position) : fieldName
-      }
-
-      return {
-        ...acc,
-        [sectionName]: isEmpty(acc[sectionName]) ? [formattedError] : uniqBy([...acc[sectionName], formattedError], 'fieldName')
-      }
-
-    }, {})
-
-    return r;
+  private groupBySections(localizedErrors: LocalizedErrors): GrouppedErrors {
+    return reduce(localizedErrors, (acc, error) => ({
+      ...acc,
+      //[error.sectionName]: uniqBy(concat(get(acc, error.sectionName, []), error), 'fieldName')
+      [error.sectionName]: flow([
+        () => get(acc, error.sectionName, []),
+        (errors) => concat(errors, error),
+        (errors) => uniqBy(errors, 'fieldName')
+      ])()
+    }), {});
   }
 
-  private localize(formattedErrors) {
-
-    console.log()
-    const r = reduce(formattedErrors, (acc, current) => {
-
-      return {
-       // ...acc,
-       // [sectionName]: isEmpty(acc[sectionName]) ? [formattedError] : uniqBy([...acc[sectionName], formattedError], 'fieldName')
-      }
-
-    }, {})
-  }
-
-  //private localizaCustomData
-
-  private localize2(errors, position) { 
-    const localized = map(errors, ({path, strategy}) => {
-
-      return {
-        description: this.localizeStrategy(strategy),
-        sectionName: this.localizeSection(path),
-        fieldName: (
-          includes(path, 'positionCustomData')
-            ? this.getCustomDataFieldName(path, position)
-            : this.localizeField(path)
-        )
-      }
-    })
+  private buildToSingleMessage(grouppedErrors) {
+    return reduce(grouppedErrors, (message, errors, groupName) => (
+      `${message}
+        <div>
+          <strong>${groupName}: </stong> 
+          ${join(reduce(errors, (acc, {fieldName, description}) => ([...acc,`"${fieldName}" ${description}`]), []), ', ')}
+        </div>`
+    ), '');
   }
 
   private getCustomDataFieldName(path: string, position: Position): string {
-    return get(replace(path, 'value', 'name'), position);
+    return get(position, replace(path, 'value', 'name'));
   }
 
   private localizeStrategy(strategy: string): string {
-    return this.propertyFilter(`html.multiplePositions.editor.validation.strategies.${strategy}`);
+    //return this.propertyFilter(`html.multiplePositions.editor.validation.strategies.${strategy}`);
+    return this.propertyFilter(`${strategy}`);
   }
 
   private localizeSection(path: string): string {
     return flow([
       () => split(path, '.'),
       (splittedPath) => first(splittedPath),
-      (sectionName) => this.propertyFilter(`html.multiplePositions.editor.validation.sections.${sectionName}`)
+      //(sectionName) => this.propertyFilter(`html.multiplePositions.editor.validation.sections.${sectionName}`)
+      (sectionName) => this.propertyFilter(`${sectionName}`)
     ])();
   }
 
@@ -262,18 +177,19 @@ class PositionErrorsAdapter {
     return flow([
       () => split(path, '.'),
       (splittedPath) => last(splittedPath),
-      (fieldName) => this.propertyFilter(`html.multiplePositions.editor.validation.fields.${fieldName}`)
+      //(fieldName) => this.propertyFilter(`html.multiplePositions.editor.validation.fields.${fieldName}`)
+      (fieldName) => this.propertyFilter(`${fieldName}`)
     ])();
   }
 }
 
 
-const service = new PositionErrorsAdapter();
+const pea = new PositionErrorsAdapter();
 
-console.log(service.localize2(validationResult, newPosition), 'service result')
+console.log(pea.transformToErrorInterface(validationResult, newPosition), 'service result')
 
 
-
+//console.log(get(newPosition, 'positionCustomData.1.name'))
 
 
 //console.log(createMapOfParents(validationResult, newPosition), 'vr')
